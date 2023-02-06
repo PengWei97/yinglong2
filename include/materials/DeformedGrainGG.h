4a9427594ebf0bc0e36e648feb7ce74d1215b88c
PengWei97 <1248110286@qq.com>
2023-02-06 15:31:21 +0800

    Add the function of getting GNDs based on ebsd, and simplify the code

--- include/materials/DeformedGrainGG.h
+++ include/materials/DeformedGrainGG.h
@@ -10,12 +10,13 @@
 #pragma once
 
 #include "Material.h"
+#include "EBSDReader.h"
 
 // Forward Declarations
 class GrainTrackerInterface;
 
 /**
- * Calculates The Deformation Energy associated with a specific dislocation density.
+ * Calculates the Deformation Energy associated with a specific dislocation density from EBSD data file.
  * The rest of parameters are the same as in the grain growth model
  */
 class DeformedGrainGG : public Material
@@ -28,42 +29,19 @@ public:
 protected:
   virtual void computeQpProperties();
 
-  /// total number of grains
-  const unsigned int _op_num;
-
-  /// order parameter values
-  const std::vector<const VariableValue *> _vals;
+  const unsigned int _op_num; // total number of grains
+  const std::vector<const VariableValue *> _vals; // order parameter values
 
   const Real _length_scale;
-  const Real _int_width;
   const Real _time_scale;
-  // const Real _GBMobility;
-
-  /// the average dislocation density
-  const Real _Disloc_Den;
-
-  /// the elastic modulus
-  const Real _Elas_Mod;
-
-  /// the Length of Burger's Vector
-  const Real _Burg_vec;
-
-  /// the prefactor needed to calculate the deformation energy from dislocation density
-  MaterialProperty<Real> & _beta;
-
-  /// dislocation density in grain i
-  MaterialProperty<Real> & _Disloc_Den_i;
-
-  /// the average/effective dislocation density
-  MaterialProperty<Real> & _rho_eff;
-
-  /// the deformation energy
-  MaterialProperty<Real> & _Def_Eng;
-
-  /// number of deformed grains
-  const unsigned int _deformed_grain_num;
-
-  /// Grain tracker object
-  const GrainTrackerInterface & _grain_tracker;
   const Real _JtoeV;
+  const Real _Elas_Mod; // the elastic modulus
+  const Real _Burg_vec; // the Length of Burger's Vector
+ 
+  const GrainTrackerInterface & _grain_tracker; // Grain tracker object
+  const EBSDReader & _GNDs_provider;
+
+  MaterialProperty<Real> & _beta;
+  MaterialProperty<Real> & _rho_eff; // the average/effective dislocation density
+  MaterialProperty<Real> & _Def_Eng; // the deformation energy
 };
--- src/materials/DeformedGrainGG.C
+++ src/materials/DeformedGrainGG.C
@@ -10,7 +10,7 @@
 #include "DeformedGrainGG.h"
 #include "GrainTrackerInterface.h"
 
-registerMooseObject("PhaseFieldApp", DeformedGrainGG);
+registerMooseObject("yinglongApp", DeformedGrainGG);
 
 InputParameters
 DeformedGrainGG::validParams()
@@ -18,16 +18,13 @@ DeformedGrainGG::validParams()
   InputParameters params = Material::validParams();
   params.addRequiredCoupledVarWithAutoBuild(
       "v", "var_name_base", "op_num", "Array of coupled variables");
-  params.addRequiredParam<unsigned int>("deformed_grain_num",
-                                        "Number of OP representing deformed grains");
   params.addParam<Real>("length_scale", 1.0e-9, "Length scale in m, where default is nm");
-  params.addParam<Real>("int_width", 4.0, "Diffuse Interface width in length_scale unit");
   params.addParam<Real>("time_scale", 1.0e-6, "Time scale in sec, where default is micro sec");
-  params.addParam<Real>("Disloc_Den", 9.0e15, "Dislocation Density in m^-2");
   params.addParam<Real>("Elas_Mod", 2.50e10, "Elastic Modulus in J/m^3");
   params.addParam<Real>("Burg_vec", 3.0e-10, "Length of Burger Vector in m");
   params.addRequiredParam<UserObjectName>("grain_tracker",
                                           "The GrainTracker UserObject to get values from.");
+  params.addRequiredParam<UserObjectName>("GNDs_provider", "Geometric necessary dislocation densities (GNDs) provider for EBSD reader");
   return params;
 }
 
@@ -36,17 +33,14 @@ DeformedGrainGG::DeformedGrainGG(const InputParameters & parameters)
     _op_num(coupledComponents("v")),
     _vals(coupledValues("v")),
     _length_scale(getParam<Real>("length_scale")),
-    _int_width(getParam<Real>("int_width")),
     _time_scale(getParam<Real>("time_scale")),
-    _Disloc_Den(getParam<Real>("Disloc_Den")),
     _Elas_Mod(getParam<Real>("Elas_Mod")),
     _Burg_vec(getParam<Real>("Burg_vec")),
     _beta(declareProperty<Real>("beta")),
-    _Disloc_Den_i(declareProperty<Real>("Disloc_Den_i")),
     _rho_eff(declareProperty<Real>("rho_eff")),
     _Def_Eng(declareProperty<Real>("Def_Eng")),
-    _deformed_grain_num(getParam<unsigned int>("deformed_grain_num")),
     _grain_tracker(getUserObject<GrainTrackerInterface>("grain_tracker")),
+    _GNDs_provider(getUserObject<EBSDReader>("GNDs_provider")),    
     _JtoeV(6.24150974e18) // Joule to eV conversion
 {
   if (_op_num == 0)
@@ -56,44 +50,28 @@ DeformedGrainGG::DeformedGrainGG(const InputParameters & parameters)
 void
 DeformedGrainGG::computeQpProperties()
 {
-  _Disloc_Den_i[_qp] = _Disloc_Den * (_length_scale * _length_scale);
-
-  Real rho_i;
-  Real rho0 = 0.0;
   Real SumEtai2 = 0.0;
   for (unsigned int i = 0; i < _op_num; ++i)
     SumEtai2 += (*_vals[i])[_qp] * (*_vals[i])[_qp];
 
-  // calculate effective dislocation density and assign zero dislocation densities to undeformed
-  // grains
+  // calculate effective dislocation density
   const auto & op_to_grains = _grain_tracker.getVarToFeatureVector(_current_elem->id());
 
-  // loop over active OPs
-  bool one_active = false;
+  Real rho_i;
+  Real rho0 = 0.0;
+  _rho_eff[_qp] = 0.0;
   for (MooseIndex(op_to_grains) op_index = 0; op_index < op_to_grains.size(); ++op_index)
   {
     if (op_to_grains[op_index] == FeatureFloodCount::invalid_id)
       continue;
 
-    one_active = true;
     auto grain_id = op_to_grains[op_index];
 
-    if (grain_id >= _deformed_grain_num)
-      rho_i = 0.0;
-    else
-      rho_i = _Disloc_Den_i[_qp];
+    rho_i = _GNDs_provider.getAvgData(grain_id)._custom[0]; // GNDs for each grain, 1/m^2 
     rho0 += rho_i * (*_vals[op_index])[_qp] * (*_vals[op_index])[_qp];
   }
 
-  // if (!one_active && _t_step > 0)
-  //   mooseError("No active order parameters");
-
   _rho_eff[_qp] = rho0 / SumEtai2;
-  if (_rho_eff[_qp] < 1e-9)
-  {
-    _rho_eff[_qp] = 0.0;
-    _Disloc_Den_i[_qp] = 0.0;
-  }
 
   _beta[_qp] = 0.5 * _Elas_Mod * _Burg_vec * _Burg_vec * _JtoeV * _length_scale;
 
