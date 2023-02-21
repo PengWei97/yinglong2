c27ca721c8e90ebe42e1c36d75c57c7963f19252
PengWei97 <1248110286@qq.com>
2023-02-21 11:21:19 +0800

    create GBAnisotropy1MisAng5

---
+++ src/materials/GBAnisotropy1MisAng5.C
@@ -0,0 +1,73 @@
+#pragma once
+#include "GBAnisotropy1MisAng5.h"
+
+registerMooseObject("yinglongApp", GBAnisotropy1MisAng5);
+
+InputParameters
+GBAnisotropy1MisAng5::validParams()
+{
+  InputParameters params = GBAnisotropy1MisAngBase::validParams();
+  params.addClassDescription(
+      "Computes necessary material properties for the isotropic grain growth model");
+  params.addParam<Real>("delta_theta_HAB", 15.0, "Benchmark value for GB anisotropy");
+  params.addRequiredParam<std::vector<Real>>("misori_angle", 
+                                             "the vector of the misorientation between grains for a piecewise function's independent varivable"); //  a piecewise function ~ x
+  params.addRequiredParam<std::vector<Real>>("sigma_energy",
+                                             "the vector of grain energy for a piecewise function's dependent varivable"); //  a piecewise function ~ y
+  params.addParam<Real>("GBsigma_LAGB", 0.1, "GB energy of a low angle GB");
+  params.addParam<Real>("GBsigma_HAGB", 1.0, "GB energy of a high angle GB");
+  params.addParam<Real>("GBmob_HAGB", 2.5e-6, "GB mobility of a high angle GB");
+  params.addParam<Real>("TT1_sigma", 0.1019, "initial value of twin boundary energy for {10-12} TT1 based on MD, J/m^2"); 
+  params.addParam<Real>("CT1_sigma", 0.0616, "initial value of twin boundary energy for {11-22} CT1 based on MD, J/m^2");
+  return params;  
+}
+
+GBAnisotropy1MisAng5::GBAnisotropy1MisAng5(const InputParameters & parameters)
+  : GBAnisotropy1MisAngBase(parameters),
+    _delta_theta_HAB(getParam<Real>("delta_theta_HAB")),
+    _piecewise_func_sigma(getParam<std::vector<Real>>("misori_angle"),
+                          getParam<std::vector<Real>>("sigma_energy")),
+    _GBsigma_LAGB(getParam<Real>("GBsigma_LAGB")),
+    _GBsigma_HAGB(getParam<Real>("GBsigma_HAGB")),
+    _GBmob_HAGB(getParam<Real>("GBmob_HAGB")),
+    _TT1_sigma(getParam<Real>("TT1_sigma")),
+    _CT1_sigma(getParam<Real>("CT1_sigma"))
+{
+}
+
+Real
+GBAnisotropy1MisAng5::calculatedGBEnergy(const misoriAngle_isTwining & misori_gbType)
+{
+  Real gbSigma = _matrix_sigma; 
+
+  if ((misori_gbType.misor < _delta_theta_HAB) && !_tb_anisotropy || !misori_gbType.isTwinning)
+    gbSigma = _piecewise_func_sigma.sample(misori_gbType.misor);
+  else if (_tb_anisotropy && misori_gbType.twinType == "twin_type0")
+    gbSigma = _TT1_sigma;
+  else if (_tb_anisotropy && misori_gbType.twinType == "twin_type1")
+    gbSigma = _CT1_sigma;
+  else if (misori_gbType.misor > _delta_theta_HAB)
+    gbSigma = _GBsigma_LAGB;
+
+  return gbSigma;
+}
+
+Real
+GBAnisotropy1MisAng5::calculatedGBMobility(const misoriAngle_isTwining & misori_gbType)
+{
+  const Real B = 5;
+  const Real n = 4;
+  Real gbMob = _matrix_mob;
+  const Real threshold_HAB = 15.0;
+
+  if ((misori_gbType.misor < threshold_HAB) && !_tb_anisotropy || !misori_gbType.isTwinning)
+    gbMob = _GBmob_HAGB * ((1- std::exp(-B * std::pow( misori_gbType.misor / threshold_HAB, n)))); // Eq.8
+  else if (_tb_anisotropy && misori_gbType.twinType == "twin_type0")
+    gbMob = _GBmob_HAGB * _TT1_sigma / _GBsigma_HAGB;
+  else if (_tb_anisotropy && misori_gbType.twinType == "twin_type1")
+    gbMob = _GBmob_HAGB * _CT1_sigma / _GBsigma_HAGB;
+  else if (misori_gbType.misor >= threshold_HAB)
+    gbMob = _GBmob_HAGB;
+
+  return gbMob;
+}
