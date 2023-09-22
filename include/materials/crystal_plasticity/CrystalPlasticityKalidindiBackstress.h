b1050829e09952cd23432b7312a3383c9cd3d713
PengWei97 <1248110286@qq.com>
2023-09-22 23:41:36 +0800

    add back stress term into CP model

--- include/materials/crystal_plasticity/CrystalPlasticityKalidindiBackstress.h
+++ include/materials/crystal_plasticity/CrystalPlasticityKalidindiBackstress.h
@@ -1,27 +1,26 @@
-//* This file is part of the MOOSE framework
-//* https://www.mooseframework.org
-//*
-//* All rights reserved, see COPYRIGHT for full restrictions
-//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
-//*
-//* Licensed under LGPL 2.1, please see LICENSE for details
-//* https://www.gnu.org/licenses/lgpl-2.1.html
+// pengwei ECUST 2023.09.20
+
+// Constitutive model form:
+// Kalidindi, S. R., Bronkhorst, C. A., & Anand, L. (1992). Crystallographic texture evolution in bulk deformation processing of FCC metals. Journal of the Mechanics and Physics of Solids, 40(3), 537-569.
+
+// Plus an Armstrong-Frederick backstress term:
+// Armstrong, P. J., & Frederick, C. O. (1966). A mathematical representation of the multiaxial Bauschinger effect (Vol. 731). Berkeley, CA: Berkeley Nuclear Laboratories.
+
+// the flow rule of the shearing slip rate\:
+// \dot{\gamma}^\alpha=\dot{\gamma}_o\left|\frac{\tau^\alpha-\chi^\alpha}{g^\alpha}\right|^M \operatorname{sgn}\left(\tau^\alpha-\chi^\alpha\right)
+
+// Armstrong-Frederick backstress term:
+// \dot{\chi}^\alpha=h \dot{\gamma}^\alpha-r \chi^\alpha\left|\dot{\gamma}^\alpha\right|
+
+// https://github.com/ngrilli/c_pfor_am/blame/main/src/materials/FiniteStrainCrystalPlasticityBackstress.C
 
 #pragma once
 
-#include "CrystalPlasticityStressUpdateBase.h"
+#include "CrystalPlasticityKalidindiUpdate.h"
 
-class CrystalPlasticityKalidindiBackstress;
+class CrystalPlasticityKalidindiUpdate;
 
-/**
- * CrystalPlasticityKalidindiBackstress uses the multiplicative decomposition of the
- * deformation gradient and solves the PK2 stress residual equation at the
- * intermediate configuration to evolve the material state. The internal
- * variables are updated using an interative predictor-corrector algorithm.
- * Backward Euler integration rule is used for the rate equations.
- */
-
-class CrystalPlasticityKalidindiBackstress : public CrystalPlasticityStressUpdateBase
+class CrystalPlasticityKalidindiBackstress : public CrystalPlasticityKalidindiUpdate
 {
 public:
   static InputParameters validParams();
@@ -31,112 +30,27 @@ public:
 protected:
   /**
    * initializes the stateful properties such as
-   * stress, plastic deformation gradient, slip system resistances, etc.
+   * stress, plastic deformation gradient, slip system resistances, back stress, etc.
    */
   virtual void initQpStatefulProperties() override;
 
-  /**
-   * Sets the value of the current and previous substep iteration slip system
-   * resistance to the old value at the start of the PK2 stress convergence
-   * while loop.
-   */
   virtual void setInitialConstitutiveVariableValues() override;
 
-  /**
-   * Sets the current slip system resistance value to the previous substep value.
-   * In cases where only one substep is taken (or when the first) substep is taken,
-   * this method just sets the current value to the old slip system resistance
-   * value again.
-   */
   virtual void setSubstepConstitutiveVariableValues() override;
-
-  /**
-   * Stores the current value of the slip system resistance into a separate
-   * material property in case substepping is needed.
-   */
-  virtual void updateSubstepConstitutiveVariableValues() override;
 
   virtual bool calculateSlipRate() override;
 
-  virtual void
-  calculateEquivalentSlipIncrement(RankTwoTensor & /*equivalent_slip_increment*/) override;
-
   virtual void calculateConstitutiveSlipDerivative(std::vector<Real> & dslip_dtau) override;
 
-  // Cache the slip system value before the update for the diff in the convergence check
-  virtual void cacheStateVariablesBeforeUpdate() override;
-
-  /**
-   * Following the Constitutive model for slip system resistance as given in
-   * Kalidindi, S.R., C.A. Bronkhorst, and L. Anand. Crystallographic texture
-   * evolution in bulk deformation processing of FCC metals. Journal of the
-   * Mechanics and Physics of Solids 40, no. 3 (1992): 537-569. Eqns 40 - 43.
-   * The slip system resistant increment is calculated as
-   * $\Delta g = \left| \Delta \gamma \cdot q^{\alpha \beta} \cdot h^{\beta} \right|$
-   * and a convergence check is performed on the slip system resistance increment
-   */
-  virtual void calculateStateVariableEvolutionRateComponent() override;
-
-  /*
-   * Finalizes the values of the state variables and slip system resistance
-   * for the current timestep after convergence has been reached.
-   */
   virtual bool updateStateVariables() override;
 
-  /*
-   * Determines if the state variables, e.g. defect densities, have converged
-   * by comparing the change in the values over the iteration period.
-   */
-  virtual bool areConstitutiveStateVariablesConverged() override;
+  // This function updates the backstress
+  virtual void updateBackstress();
 
-  ///@{Varibles used in the Kalidindi 1992 slip system resistance constiutive model
-  const Real _r;
-  const Real _h;
-  const Real _tau_sat;
-  const Real _gss_a;
-  const Real _ao;
-  const Real _xm;
-  const Real _gss_initial;
-  ///@}
+  const Real _c_backstress;
+  const Real _d_backstress;
 
-  /**
-   * Slip system interaction matrix used to calculate the hardening contributions
-   * from the self and latent slip systems, from Kalidindi et al (1992).
-   */
-  std::vector<Real> _hb;
-
-  /// Increment of increased resistance for each slip system
-  std::vector<Real> _slip_resistance_increment;
-
-  /**
-   * Stores the values of the slip system resistance from the previous substep
-   * In classes which use dislocation densities, analogous dislocation density
-   * substep vectors will be required.
-   */
-  std::vector<Real> _previous_substep_slip_resistance;
-
-  /**
-   * Caches the value of the current slip system resistance immediately prior
-   * to the update of the slip system resistance, and is used to calculate the
-   * the slip system resistance increment for the current substep (or step if
-   * only one substep is taken) for the convergence check tolerance comparison.
-   * In classes which use dislocation densities, analogous dislocation density
-   * caching vectors will also be required.
-   */
-  std::vector<Real> _slip_resistance_before_update;
-
-  /**
-   * Flag to include the total twin volume fraction in the plastic velocity
-   * gradient calculation, per Kalidindi IJP (2001).
-   */
-  const bool _include_twinning_in_Lp;
-
-  /**
-   * User-defined material property name for the total volume fraction of twins
-   * in a twinning propagation constitutive model, when this class is used in
-   * conjunction with the twinning propagation model.
-   * Note that this value is the OLD material property and thus lags the current
-   * value by a single timestep.
-   */
-  const MaterialProperty<Real> * const _twin_volume_fraction_total;
+  // Backstress for each slip system
+  MaterialProperty<std::vector<Real>> & _tau_b;
+  const MaterialProperty<std::vector<Real>> & _tau_b_old;
 };
--- src/materials/crystal_plasticity/CrystalPlasticityKalidindiBackstress.C
+++ src/materials/crystal_plasticity/CrystalPlasticityKalidindiBackstress.C
@@ -1,101 +1,84 @@
-//* This file is part of the MOOSE framework
-//* https://www.mooseframework.org
-//*
-//* All rights reserved, see COPYRIGHT for full restrictions
-//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
-//*
-//* Licensed under LGPL 2.1, please see LICENSE for details
-//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+// pengwei ECUST 2023.09.20
+
+// Constitutive model form:
+// Kalidindi, S. R., Bronkhorst, C. A., & Anand, L. (1992). Crystallographic texture evolution in bulk deformation processing of FCC metals. Journal of the Mechanics and Physics of Solids, 40(3), 537-569.
+
+// Plus an Armstrong-Frederick backstress term:
+// Armstrong, P. J., & Frederick, C. O. (1966). A mathematical representation of the multiaxial Bauschinger effect (Vol. 731). Berkeley, CA: Berkeley Nuclear Laboratories.
+
+// the flow rule of the shearing slip rate\:
+// \dot{\gamma}^\alpha=\dot{\gamma}_o\left|\frac{\tau^\alpha-\chi^\alpha}{g^\alpha}\right|^M \operatorname{sgn}\left(\tau^\alpha-\chi^\alpha\right)
+
+// Armstrong-Frederick backstress term:
+// \dot{\chi}^\alpha=h \dot{\gamma}^\alpha-r \chi^\alpha\left|\dot{\gamma}^\alpha\right|
+
+// https://github.com/ngrilli/c_pfor_am/blame/main/src/materials/FiniteStrainCrystalPlasticityBackstress.C
 
 #include "CrystalPlasticityKalidindiBackstress.h"
 #include "libmesh/int_range.h"
 
-registerMooseObject("TensorMechanicsApp", CrystalPlasticityKalidindiBackstress);
+registerMooseObject("yinglongApp", CrystalPlasticityKalidindiBackstress);
 
 InputParameters
 CrystalPlasticityKalidindiBackstress::validParams()
 {
-  InputParameters params = CrystalPlasticityStressUpdateBase::validParams();
-  params.addClassDescription("Kalidindi version of homogeneous crystal plasticity.");
-  params.addParam<Real>("r", 1.0, "Latent hardening coefficient");
-  params.addParam<Real>("h", 541.5, "hardening constants");
-  params.addParam<Real>("t_sat", 109.8, "saturated slip system strength");
-  params.addParam<Real>("gss_a", 2.5, "coefficient for hardening");
-  params.addParam<Real>("ao", 0.001, "slip rate coefficient");
-  params.addParam<Real>("xm", 0.1, "exponent for slip rate");
-  params.addParam<Real>("gss_initial", 60.8, "initial lattice friction strength of the material");
-
-  params.addParam<MaterialPropertyName>(
-      "total_twin_volume_fraction",
-      "Total twin volume fraction, if twinning is considered in the simulation");
+  InputParameters params = CrystalPlasticityKalidindiUpdate::validParams();
+  params.addClassDescription("Kalidindi version of homogeneous crystal plasticity considering back stress for fatigure loading.");
+  params.addParam<Real>("c_backstress",0.0,"c parameter in Armstrong-Frederick backstress evolution");
+  params.addParam<Real>("d_backstress",0.0,"d parameter in Armstrong-Frederick backstress evolution");
 
   return params;
 }
 
 CrystalPlasticityKalidindiBackstress::CrystalPlasticityKalidindiBackstress(
     const InputParameters & parameters)
-  : CrystalPlasticityStressUpdateBase(parameters),
-    // Constitutive values
-    _r(getParam<Real>("r")),
-    _h(getParam<Real>("h")),
-    _tau_sat(getParam<Real>("t_sat")),
-    _gss_a(getParam<Real>("gss_a")),
-    _ao(getParam<Real>("ao")),
-    _xm(getParam<Real>("xm")),
-    _gss_initial(getParam<Real>("gss_initial")),
-
-    // resize vectors used in the consititutive slip hardening
-    _hb(_number_slip_systems, 0.0),
-    _slip_resistance_increment(_number_slip_systems, 0.0),
-
-    // resize local caching vectors used for substepping
-    _previous_substep_slip_resistance(_number_slip_systems, 0.0),
-    _slip_resistance_before_update(_number_slip_systems, 0.0),
-
-    // Twinning contributions, if used
-    _include_twinning_in_Lp(parameters.isParamValid("total_twin_volume_fraction")),
-    _twin_volume_fraction_total(_include_twinning_in_Lp
-                                    ? &getMaterialPropertyOld<Real>("total_twin_volume_fraction")
-                                    : nullptr)
+  : CrystalPlasticityKalidindiUpdate(parameters),
+    _c_backstress(getParam<Real>("c_backstress")), // c parameter in Armstrong-Frederick backstress evolution
+    _d_backstress(getParam<Real>("d_backstress")), // d parameter in Armstrong-Frederick backstress evolution
+    _tau_b(declareProperty<std::vector<Real>>("tau_b")), // Backstress for each slip system
+    _tau_b_old(getMaterialPropertyOld<std::vector<Real>>("tau_b"))
 {
 }
 
 void
 CrystalPlasticityKalidindiBackstress::initQpStatefulProperties()
 {
-  CrystalPlasticityStressUpdateBase::initQpStatefulProperties();
+  // tau, flow_direction, slip_resistance(g), slip_increment(delta_gamma^alpha), _tau_b
+  CrystalPlasticityKalidindiUpdate::initQpStatefulProperties();
 
+  _tau_b[_qp].resize(_number_slip_systems);
   for (const auto i : make_range(_number_slip_systems))
-  {
-    _slip_resistance[_qp][i] = _gss_initial;
-    _slip_increment[_qp][i] = 0.0;
-  }
+    _tau_b[_qp][i] = 0.0; // initialise back stress
 }
 
 void
 CrystalPlasticityKalidindiBackstress::setInitialConstitutiveVariableValues()
 {
-  // Would also set old dislocation densities here if included in this model
-  _slip_resistance[_qp] = _slip_resistance_old[_qp];
-  _previous_substep_slip_resistance = _slip_resistance_old[_qp];
+ CrystalPlasticityKalidindiUpdate::setInitialConstitutiveVariableValues();
+
+  _tau_b[_qp] = _tau_b_old[_qp];
 }
 
 void
 CrystalPlasticityKalidindiBackstress::setSubstepConstitutiveVariableValues()
 {
-  // Would also set substepped dislocation densities here if included in this model
-  _slip_resistance[_qp] = _previous_substep_slip_resistance;
+  CrystalPlasticityKalidindiUpdate::setSubstepConstitutiveVariableValues();
+
+  _tau_b[_qp] = _tau_b_old[_qp];
 }
 
 bool
 CrystalPlasticityKalidindiBackstress::calculateSlipRate()
 {
+  Real tau_b; // Backstress: temporary variable
+
   for (const auto i : make_range(_number_slip_systems))
   {
+    tau_b = _tau_b[_qp][i];
+
     _slip_increment[_qp][i] =
-        _ao * std::pow(std::abs(_tau[_qp][i] / _slip_resistance[_qp][i]), 1.0 / _xm);
-    if (_tau[_qp][i] < 0.0)
-      _slip_increment[_qp][i] *= -1.0;
+        _ao * std::pow(std::abs((_tau[_qp][i] - tau_b) / _slip_resistance[_qp][i]), 1.0 / _xm) * std::copysign(1.0, (_tau[_qp][i] - tau_b));
 
     if (std::abs(_slip_increment[_qp][i]) * _substep_dt > _slip_incr_tol)
     {
@@ -110,87 +93,36 @@ CrystalPlasticityKalidindiBackstress::calculateSlipRate()
 }
 
 void
-CrystalPlasticityKalidindiBackstress::calculateEquivalentSlipIncrement(
-    RankTwoTensor & equivalent_slip_increment)
-{
-  if (_include_twinning_in_Lp)
-  {
-    for (const auto i : make_range(_number_slip_systems))
-      equivalent_slip_increment += (1.0 - (*_twin_volume_fraction_total)[_qp]) *
-                                   _flow_direction[_qp][i] * _slip_increment[_qp][i] * _substep_dt;
-  }
-  else // if no twinning volume fraction material property supplied, use base class
-    CrystalPlasticityStressUpdateBase::calculateEquivalentSlipIncrement(equivalent_slip_increment);
-}
-
-void
 CrystalPlasticityKalidindiBackstress::calculateConstitutiveSlipDerivative(
     std::vector<Real> & dslip_dtau)
 {
+  Real tau_b; // Backstress: temporary variable
+
   for (const auto i : make_range(_number_slip_systems))
   {
+    tau_b = _tau_b[_qp][i];
+
     if (MooseUtils::absoluteFuzzyEqual(_tau[_qp][i], 0.0))
       dslip_dtau[i] = 0.0;
     else
-      dslip_dtau[i] = _ao / _xm *
-                      std::pow(std::abs(_tau[_qp][i] / _slip_resistance[_qp][i]), 1.0 / _xm - 1.0) /
+      dslip_dtau[i] = _ao / _xm * 
+                      std::pow(std::abs((_tau[_qp][i] - tau_b) / _slip_resistance[_qp][i]), 1.0 / _xm - 1.0) /
                       _slip_resistance[_qp][i];
   }
 }
 
-bool
-CrystalPlasticityKalidindiBackstress::areConstitutiveStateVariablesConverged()
-{
-  return isConstitutiveStateVariableConverged(_slip_resistance[_qp],
-                                              _slip_resistance_before_update,
-                                              _previous_substep_slip_resistance,
-                                              _resistance_tol);
-}
+// void
+// CrystalPlasticityKalidindiBackstress::updateSubstepConstitutiveVariableValues()
+// {
+//   // Would also set substepped dislocation densities here if included in this model
+//   _previous_substep_slip_resistance = _slip_resistance[_qp];
+// }
 
-void
-CrystalPlasticityKalidindiBackstress::updateSubstepConstitutiveVariableValues()
-{
-  // Would also set substepped dislocation densities here if included in this model
-  _previous_substep_slip_resistance = _slip_resistance[_qp];
-}
-
-void
-CrystalPlasticityKalidindiBackstress::cacheStateVariablesBeforeUpdate()
-{
-  _slip_resistance_before_update = _slip_resistance[_qp];
-}
-
-void
-CrystalPlasticityKalidindiBackstress::calculateStateVariableEvolutionRateComponent()
-{
-  for (const auto i : make_range(_number_slip_systems))
-  {
-    // Clear out increment from the previous iteration
-    _slip_resistance_increment[i] = 0.0;
-
-    _hb[i] = _h * std::pow(std::abs(1.0 - _slip_resistance[_qp][i] / _tau_sat), _gss_a);
-    const Real hsign = 1.0 - _slip_resistance[_qp][i] / _tau_sat;
-    if (hsign < 0.0)
-      _hb[i] *= -1.0;
-  }
-
-  for (const auto i : make_range(_number_slip_systems))
-  {
-    for (const auto j : make_range(_number_slip_systems))
-    {
-      unsigned int iplane, jplane;
-      iplane = i / 3;
-      jplane = j / 3;
-
-      if (iplane == jplane) // self vs. latent hardening
-        _slip_resistance_increment[i] +=
-            std::abs(_slip_increment[_qp][j]) * _hb[j]; // q_{ab} = 1.0 for self hardening
-      else
-        _slip_resistance_increment[i] +=
-            std::abs(_slip_increment[_qp][j]) * _r * _hb[j]; // latent hardenign
-    }
-  }
-}
+// void
+// CrystalPlasticityKalidindiBackstress::cacheStateVariablesBeforeUpdate()
+// {
+//   _slip_resistance_before_update = _slip_resistance[_qp];
+// }
 
 bool
 CrystalPlasticityKalidindiBackstress::updateStateVariables()
@@ -199,14 +131,41 @@ CrystalPlasticityKalidindiBackstress::updateStateVariables()
   for (const auto i : make_range(_number_slip_systems))
   {
     _slip_resistance_increment[i] *= _substep_dt;
+
     if (_previous_substep_slip_resistance[i] < _zero_tol && _slip_resistance_increment[i] < 0.0)
       _slip_resistance[_qp][i] = _previous_substep_slip_resistance[i];
     else
       _slip_resistance[_qp][i] =
           _previous_substep_slip_resistance[i] + _slip_resistance_increment[i];
 
+    updateBackstress();
+
     if (_slip_resistance[_qp][i] < 0.0)
       return false;
   }
   return true;
+}
+
+// Update of the Armstrong-Frederick backstress term:
+// Armstrong, P.J., Frederick, C.O., 1966. 
+// A Mathematical Representation of the Multiaxial Bauschinger Effect, 
+// G.E.G.B. Report RD/B/N. Central Electricity Generating Board
+// Backstress term can be changed by changing this function only
+void
+CrystalPlasticityKalidindiBackstress::updateBackstress()
+{
+  Real dtau_b; // increment of backstress, temporary variable
+
+  for (const auto i : make_range(_number_slip_systems))
+    _tau_b[i] = _tau_b_old[i];
+
+  // _dt here is the substep
+  for (const auto i : make_range(_number_slip_systems))
+  {	  
+    dtau_b = 0.0;
+    dtau_b = _c_backstress * _slip_resistance_increment[i];
+    dtau_b -= _d_backstress * _tau_b[_qp][i] * std::abs(_slip_resistance_increment[i]);
+      
+    _tau_b[_qp][i] += dtau_b;
+  }
 }
